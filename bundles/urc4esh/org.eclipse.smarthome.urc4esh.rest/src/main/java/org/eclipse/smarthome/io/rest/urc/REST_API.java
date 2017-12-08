04ce6732efbeee37d770bc0a53ce079de258ecbd
Lukas84 <smirek@hdm-stuttgart.de>
2017-12-08 11:33:17 +0100

    org.eclipse.smarthome.urc4esh.rst added

---
+++ bundles/urc4esh/org.eclipse.smarthome.urc4esh.rest/src/main/java/org/eclipse/smarthome/io/rest/urc/REST_API.java
@@ -0,0 +1,6 @@
+package org.eclipse.smarthome.io.rest.urc;
+
+public class REST_API {
+    public static final String USERCONTEXT_WITH_ID = null;
+    public final String ENVIRONMENT_cCONTEXT_DEFINITION = "environment-context-definition";
+}
