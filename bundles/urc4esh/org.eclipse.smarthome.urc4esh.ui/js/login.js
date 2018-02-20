dad2c3b3506a7072529212d78585d02b76668edb
Lukas84 <smirek@hdm-stuttgart.de>
2018-02-20 09:41:15 +0100

    modified build.properties for API and communication manager

--- bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/login.js
+++ bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/login.js
@@ -1,3 +1,15 @@
 /**
  * 
  */
+
+$('btnLogIn').click( function(){
+   var data;
+   data.username = $.("iptUsername").getValue();
+   data.password = $.("iptPassword").getValue();
+   data = JSON.stringify(data);
+    $.post("rest/urc/login", data) 
+});
+
+$('btnRegisterOpenApe').click( function(){
+    
+});
