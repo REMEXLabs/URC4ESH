be8e1c23bbc60f6c7fe0bc822366b5fb19d3f91e
Lukas84 <smirek@hdm-stuttgart.de>
2018-02-22 17:06:49 +0100

    starting with Urc4eshService

--- bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/login.js
+++ bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/login.js
@@ -1,15 +1,14 @@
 /**
  * 
  */
-
-$('btnLogIn').click( function(){
-   var data= {};
-   data.username = $.("iptUsername").getValue();
-   data.password = $.("iptPassword").getValue();
-   data = JSON.stringify(data);
-    $.post("rest/urc/login", data) 
-});
-
-$('btnRegisterOpenApe').click( function(){
-    
-});
+/*
+ * $('btnLogIn').click( function(){ var data = {}; data.username = $.("iptUsername").getValue(); data.password =
+ * $.("iptPassword").getValue(); data = JSON.stringify(data); $.post("rest/urc/login", data) .done(function(response){
+ * console.log("erfolg");
+ * 
+ * }); console.log("Login requested"); });
+ * 
+ * $('btnRegisterOpenApe').click( function(){
+ * 
+ * });
+ */
