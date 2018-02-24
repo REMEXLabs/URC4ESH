90661174ef151a433b5ba9bebac926bd192018ff
Lukas84 <smirek@hdm-stuttgart.de>
2018-02-24 19:37:11 +0100

    Communicator as Interface for OpenApeClient Added

--- bundles/urc4esh/org.eclipse.smarthome.urc4esh.api/src/main/java/org/eclipse/smarthome/urc4esh/api/CommunicationManager.java
+++ bundles/urc4esh/org.eclipse.smarthome.urc4esh.api/src/main/java/org/eclipse/smarthome/urc4esh/api/CommunicationManager.java
@@ -1,9 +1,7 @@
 package org.eclipse.smarthome.urc4esh.api;
 
-import org.openape.client.OpenAPEClient;
-
 public interface CommunicationManager {
-    public static OpenAPEClient getClient(String Username, String password) {
+    public static Communicator getClient(String Username, String password) {
         // TODO Auto-generated method stub
         return null;
     }
