fcac2f24d64afe3536705bbaa263ec386f146643
Lukas84 <smirek@hdm-stuttgart.de>
2018-02-28 11:12:11 +0100

    creates tableEquipmentContext

--- bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/ViewController.js
+++ bundles/urc4esh/org.eclipse.smarthome.urc4esh.ui/js/ViewController.js
@@ -4,11 +4,50 @@
 var ViewController = {
 
     addThings : function(htmlElement) {
+// $(htmlElement).append($( "<tr> <td> Thing UID </td></tr>") );
+        EshCommunicator.getAllThings(function(thingList , status) {
 
-        EshCommunicator.getAllThings(function(data, status) {
-            console.log("Status: " + status)
-            console.log("data: " + data.responseText)
-            var things = JSON.parse(data);
-        });
-    }
-}
+//console.log("EL: " +  $(htmlElement) )
+//$("body").append( "<p> Hello World </p>" );
+thingList.forEach(function  (item,index, array) {
+console.log("tabellenaufbau");
+/*
+var mytbl = document.getElementById('tableEquipmentContext');
+mytbl.appendChild(document.createElement('tr'));
+*/
+var tr  = document.createElement('tr');
+
+//tr.appendChild( $('<td/>').append( $('<input type="checkbox"/>')  ) );
+
+var td_checkbox = document.createElement('td');
+var checkbox = document.createElement('input');
+checkbox.setAttribute('id', 'cbx' + item.label);
+checkbox.setAttribute('type', 'checkbox');
+tr.appendChild(checkbox);
+var td_uid = document.createElement('td');
+var text_uid = document.createTextNode( item.UID );
+td_uid.appendChild(text_uid );
+tr.appendChild( td_uid );
+
+var td_label = document.createElement('td');
+var text_label = document.createTextNode( item.label );
+td_label.appendChild( text_label );
+tr.appendChild( td_label );
+
+var td_thingType = document.createElement('td');
+td_thingType.appendChild( document.createTextNode(  item.thingTypeUID));
+tr.appendChild(td_thingType);
+
+ document.getElementById(htmlElement).appendChild(tr);
+
+ 
+}); // for each
+
+
+}); // getAllThings
+            }  // AddThings
+}  // view controller
+
+$('iptExpertLevel').click(function() { 
+console.log("klick expert level");
+});
